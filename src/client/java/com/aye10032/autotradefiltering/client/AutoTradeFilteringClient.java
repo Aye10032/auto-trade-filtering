fb76d988c3e2b00144873962700935c8505c1594
Aye10032 <tc_aye@outlook.com>
2026-05-01 21:19:44 +0800

    build: init project

---
+++ src/client/java/com/aye10032/autotradefiltering/client/AutoTradeFilteringClient.java
@@ -0,0 +1,10 @@
+package com.aye10032.autotradefiltering.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutoTradeFilteringClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
